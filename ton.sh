e71e49e117aba963a7ba23f211efd33747879864
khavandang <110210584+khavandang@users.noreply.github.com>
2022-08-04 10:50:48 +0700

    Create ton.sh

---
+++ ton.sh
@@ -0,0 +1,4 @@
+! wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.48/lolMiner_v1.48_Lin64.tar.gz
+! tar -xf lolMiner_v1.48_Lin64.tar.gz
+! cd 1.48
+! ./lolMiner -a TON --pool https://server1.whalestonpool.com --user EQCWV_YcgNZmO5U5KOzJmLCcYy_Iizfk1LuXGRz7ruoxDwr6
